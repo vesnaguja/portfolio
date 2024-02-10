5cfd52f849963ad3fd7111c23e179f65862966ae
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2024-02-10 09:59:28 +0000

    Deploying to gh-pages from @ vesnaguja/portfolio@d16fa9674792228716fbce9858cc0c6538960ee5 🚀

--- README.md
+++
@@ -1,7 +0,0 @@
-# Portfolio 
-
-Purpose of this site:
-- providing information about me
-- practicing Context
-
-Site is created using React, JavaScript, HTML5, Bootstrap5 and CSS3.
--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="container-fluid mt-5">
-      <div className="container text-center p-3">
-        © 2021 - {new Date().getFullYear()} Copyright: Vesna Grujanić
-      </div>
-    </footer>
-  );
-};
